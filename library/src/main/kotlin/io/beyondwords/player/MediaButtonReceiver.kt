8d67f7aa3fc6e6da7b6f04b2ae3a0c748404d423
nklhtv <nklhtv@gmail.com>
2023-05-29 19:26:36 +0300

    media session shim added

---
+++ library/src/main/kotlin/io/beyondwords/player/MediaButtonReceiver.kt
@@ -0,0 +1,56 @@
+package io.beyondwords.player
+
+import android.app.PendingIntent
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import android.support.v4.media.session.PlaybackStateCompat
+import android.view.KeyEvent
+
+open class MediaButtonReceiver(private val mediaSessionId: Int) : BroadcastReceiver() {
+    companion object {
+        const val EXTRA_MEDIA_SESSION_ID = "io.beyondwords.player.MEDIA_SESSION_ID"
+
+        fun buildMediaButtonPendingIntent(
+            context: Context,
+            mediaSessionId: Int,
+            action: Long
+        ): PendingIntent? {
+            val keyCode = PlaybackStateCompat.toKeyCode(action)
+            if (keyCode == KeyEvent.KEYCODE_UNKNOWN) {
+                return null
+            }
+
+            val intent = Intent(Intent.ACTION_MEDIA_BUTTON)
+            intent.putExtra(Intent.EXTRA_KEY_EVENT, KeyEvent(KeyEvent.ACTION_DOWN, keyCode))
+            intent.putExtra(EXTRA_MEDIA_SESSION_ID, mediaSessionId)
+            val requestCode = mediaSessionId * 1000 + keyCode // keyCode is a 3 digit number
+            val flags = if (Build.VERSION.SDK_INT >= 31) PendingIntent.FLAG_MUTABLE else 0
+            return PendingIntent.getBroadcast(context, requestCode, intent, flags)
+        }
+    }
+
+    override fun onReceive(context: Context?, intent: Intent?) {
+        if (intent === null
+            || intent.action != Intent.ACTION_MEDIA_BUTTON
+            || !intent.hasExtra(Intent.EXTRA_KEY_EVENT)
+            || intent.getIntExtra(EXTRA_MEDIA_SESSION_ID, 0) != mediaSessionId
+        ) return
+        val event: KeyEvent? = intent.getParcelableExtra(Intent.EXTRA_KEY_EVENT) as KeyEvent?
+        when (event?.keyCode) {
+            KeyEvent.KEYCODE_MEDIA_PLAY -> onPlay()
+            KeyEvent.KEYCODE_MEDIA_PAUSE -> onPause()
+            KeyEvent.KEYCODE_MEDIA_PREVIOUS -> onSkipToPrevious()
+            KeyEvent.KEYCODE_MEDIA_NEXT -> onSkipToNext()
+        }
+    }
+
+    open fun onPlay() {}
+
+    open fun onPause() {}
+
+    open fun onSkipToNext() {}
+
+    open fun onSkipToPrevious() {}
+}
--- library/src/main/kotlin/io/beyondwords/player/PlayerView.kt
+++ library/src/main/kotlin/io/beyondwords/player/PlayerView.kt
@@ -2,10 +2,15 @@ package io.beyondwords.player
 
 import android.annotation.SuppressLint
 import android.app.DownloadManager
+import android.app.NotificationManager
 import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
 import android.graphics.Color
 import android.net.Uri
+import android.support.v4.media.MediaMetadataCompat
+import android.support.v4.media.session.MediaSessionCompat
+import android.support.v4.media.session.PlaybackStateCompat
 import android.util.AttributeSet
 import android.util.Log
 import android.util.TypedValue
@@ -16,11 +21,17 @@ import android.webkit.WebSettings.LOAD_NO_CACHE
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import android.widget.FrameLayout
+import androidx.core.app.NotificationChannelCompat
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import androidx.core.content.ContextCompat
 import androidx.core.view.updateLayoutParams
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
+import java.util.concurrent.atomic.AtomicInteger
 
+//https://developer.android.com/about/versions/oreo/android-8.0-changes.html
+//https://developer.android.com/about/versions/13/behavior-changes-13#playback-controls
 @SuppressLint("SetJavaScriptEnabled")
 class PlayerView @JvmOverloads constructor(
     context: Context,
@@ -29,12 +40,37 @@ class PlayerView @JvmOverloads constructor(
     defStyleRes: Int = 0
 ) : FrameLayout(context, attrs, defStyleAttr, defStyleRes) {
     companion object {
-        private val gson: Gson by lazy {
-            GsonBuilder().create()
+        const val DEFAULT_NOTIFICATION_CHANNEL_ID = "BeyondWords"
+
+        private val currentMediaSessionId = AtomicInteger(0)
+        private val gson: Gson by lazy { GsonBuilder().create() }
+
+        var notificationChannelId: String? = null
+
+        private fun registerNotificationChannel(context: Context) {
+            if (notificationChannelId != null) return
+            val channel = NotificationChannelCompat.Builder(
+                DEFAULT_NOTIFICATION_CHANNEL_ID,
+                NotificationManagerCompat.IMPORTANCE_LOW
+            )
+                .setName("BeyondWords")
+                .setDescription("BeyondWords audio player")
+                .setLightsEnabled(false)
+                .setShowBadge(false)
+                .setVibrationEnabled(false)
+                .setSound(null, null)
+                .build()
+            NotificationManagerCompat.from(context)
+                .createNotificationChannel(channel)
         }
     }
 
     private var ready = false
+    private val mediaSessionId = currentMediaSessionId.getAndIncrement()
+    private var mediaMetadata: MediaMetadataCompat? = null
+    private var playbackState: PlaybackStateCompat? = null
+    private var mediaSession: MediaSessionCompat? = null
+    private var mediaButtonReceiver: MediaButtonReceiver? = null
     private val webViewContainer = FrameLayout(context)
     private val webView = WebView(context)
     private val listeners = mutableSetOf<EventListener>()
@@ -51,6 +87,7 @@ class PlayerView @JvmOverloads constructor(
             }
         }
 
+        @Suppress("UNUSED_PARAMETER")
         @JavascriptInterface
         fun onResize(width: Int, height: Int) {
             webViewContainer.post {
@@ -81,6 +118,66 @@ class PlayerView @JvmOverloads constructor(
                     }
                     it.onAny(parsedEvent)
                 }
+            }
+        }
+
+        @JavascriptInterface
+        fun onMetadataChanged(title: String?, artist: String?, album: String?, artwork: String?) {
+            post {
+                val mediaMetadataBuilder = mediaMetadata?.let {
+                    MediaMetadataCompat.Builder(it)
+                } ?: run {
+                    MediaMetadataCompat.Builder()
+                }
+                mediaMetadataBuilder.putString(MediaMetadataCompat.METADATA_KEY_TITLE, title)
+                mediaMetadataBuilder.putString(MediaMetadataCompat.METADATA_KEY_ARTIST, artist)
+                mediaMetadataBuilder.putString(MediaMetadataCompat.METADATA_KEY_ALBUM, album)
+                mediaMetadataBuilder.putString(
+                    MediaMetadataCompat.METADATA_KEY_ALBUM_ART_URI,
+                    artwork
+                )
+                mediaMetadata = mediaMetadataBuilder.build()
+                updateMediaSession()
+            }
+        }
+
+        @JavascriptInterface
+        fun onPlaybackStateChanged(
+            state: String,
+            position: Float,
+            duration: Float,
+            playbackSpeed: Float
+        ) {
+            post {
+                val mediaMetadataBuilder = mediaMetadata?.let {
+                    MediaMetadataCompat.Builder(it)
+                } ?: run {
+                    MediaMetadataCompat.Builder()
+                }
+                mediaMetadataBuilder.putLong(
+                    MediaMetadataCompat.METADATA_KEY_DURATION,
+                    (duration * 1000).toLong()
+                )
+                val playbackStateBuilder = PlaybackStateCompat.Builder()
+                    .setState(
+                        when (state) {
+                            "playing" -> PlaybackStateCompat.STATE_PLAYING
+                            "paused" -> PlaybackStateCompat.STATE_PAUSED
+                            else -> PlaybackStateCompat.STATE_NONE
+                        },
+                        (position * 1000).toLong(),
+                        playbackSpeed
+                    )
+                    .setActions(
+                        PlaybackStateCompat.ACTION_PLAY or
+                                PlaybackStateCompat.ACTION_PAUSE or
+                                PlaybackStateCompat.ACTION_SKIP_TO_NEXT or
+                                PlaybackStateCompat.ACTION_SKIP_TO_PREVIOUS or
+                                PlaybackStateCompat.ACTION_SEEK_TO
+                    )
+                playbackState = playbackStateBuilder.build()
+                mediaMetadata = mediaMetadataBuilder.build()
+                updateMediaSession()
             }
         }
     }
@@ -151,6 +248,23 @@ class PlayerView @JvmOverloads constructor(
         )
     }
 
+    override fun onAttachedToWindow() {
+        super.onAttachedToWindow()
+        updateMediaSession()
+    }
+
+    override fun onDetachedFromWindow() {
+        super.onDetachedFromWindow()
+        mediaButtonReceiver?.let {
+            context.unregisterReceiver(it)
+        }
+        mediaButtonReceiver = null
+        mediaSession?.release()
+        mediaSession = null
+        ContextCompat.getSystemService(context, NotificationManager::class.java)
+            ?.cancel(mediaSessionId)
+    }
+
     fun addEventListener(listener: EventListener) {
         listeners.add(listener)
     }
@@ -197,5 +311,132 @@ class PlayerView @JvmOverloads constructor(
         } else {
             webView.evaluateJavascript(command, null)
         }
+    }
+
+    private fun updateMediaSession() {
+        if (!isAttachedToWindow || mediaMetadata == null || playbackState == null) return
+        if (mediaButtonReceiver == null) {
+            mediaButtonReceiver = object : MediaButtonReceiver(mediaSessionId) {
+                override fun onPlay() {
+                    callFunction("navigator.mediaSession._actionHandlers.play", listOf())
+                }
+
+                override fun onPause() {
+                    callFunction("navigator.mediaSession._actionHandlers.pause", listOf())
+                }
+
+                override fun onSkipToNext() {
+                    Log.d("MediaSession", "onSkipToNext")
+                }
+
+                override fun onSkipToPrevious() {
+                    Log.d("MediaSession", "onSkipToPrevious")
+                }
+            }
+            context.registerReceiver(mediaButtonReceiver, IntentFilter(Intent.ACTION_MEDIA_BUTTON))
+        }
+        if (mediaSession == null) {
+            mediaSession = MediaSessionCompat(context, "BeyondWords")
+            mediaSession!!.setCallback(object : MediaSessionCompat.Callback() {
+                override fun onPlay() {
+                    callFunction("navigator.mediaSession._actionHandlers.play", listOf())
+                }
+
+                override fun onPause() {
+                    callFunction("navigator.mediaSession._actionHandlers.pause", listOf())
+                }
+
+                override fun onSkipToNext() {
+                    Log.d("MediaSession", "onSkipToNext")
+                }
+
+                override fun onSkipToPrevious() {
+                    Log.d("MediaSession", "onSkipToPrevious")
+                }
+
+                override fun onSeekTo(position: Long) {
+                    callFunction("navigator.mediaSession._actionHandlers.seekto", listOf(object {
+                        val seekTime = position / 1000
+                    }))
+                }
+            })
+        }
+        mediaSession!!.setMetadata(mediaMetadata)
+        mediaSession!!.setPlaybackState(playbackState)
+        mediaSession!!.isActive = true
+        registerNotificationChannel(context)
+        updateNotification()
+    }
+
+    private fun updateNotification() {
+        val metadata = mediaSession?.controller?.metadata ?: return
+        val playbackState = mediaSession?.controller?.playbackState ?: return
+
+        if (playbackState.state != PlaybackStateCompat.STATE_PLAYING &&
+            playbackState.state != PlaybackStateCompat.STATE_PAUSED
+        ) return
+
+        val notificationBuilder = NotificationCompat.Builder(
+            context,
+            notificationChannelId ?: DEFAULT_NOTIFICATION_CHANNEL_ID
+        )
+        notificationBuilder.setSmallIcon(R.drawable.ic_volume_up)
+        metadata.getString(MediaMetadataCompat.METADATA_KEY_TITLE)?.let {
+            notificationBuilder.setContentTitle(it)
+        }
+        notificationBuilder.addAction(
+            R.drawable.ic_skip_previous,
+            "Skip to previous",
+            MediaButtonReceiver.buildMediaButtonPendingIntent(
+                context,
+                mediaSessionId,
+                PlaybackStateCompat.ACTION_SKIP_TO_PREVIOUS
+            )
+        )
+        if (playbackState.state == PlaybackStateCompat.STATE_PLAYING) {
+            notificationBuilder.addAction(
+                R.drawable.ic_pause,
+                "Pause",
+                MediaButtonReceiver.buildMediaButtonPendingIntent(
+                    context,
+                    mediaSessionId,
+                    PlaybackStateCompat.ACTION_PAUSE
+                )
+            )
+        } else {
+            notificationBuilder.addAction(
+                R.drawable.ic_play,
+                "Play",
+                MediaButtonReceiver.buildMediaButtonPendingIntent(
+                    context,
+                    mediaSessionId,
+                    PlaybackStateCompat.ACTION_PLAY
+                )
+            )
+        }
+        notificationBuilder.addAction(
+            R.drawable.ic_skip_next,
+            "Skip to next",
+            MediaButtonReceiver.buildMediaButtonPendingIntent(
+                context,
+                mediaSessionId,
+                PlaybackStateCompat.ACTION_SKIP_TO_NEXT
+            )
+        )
+        notificationBuilder.setStyle(
+            androidx.media.app.NotificationCompat.MediaStyle()
+                .setShowActionsInCompactView(1)
+                .setMediaSession(mediaSession?.sessionToken)
+        )
+        notificationBuilder.setSilent(true)
+        notificationBuilder.setAutoCancel(false)
+        notificationBuilder.setSound(null)
+        notificationBuilder.setVibrate(null)
+        notificationBuilder.setOngoing(true)
+        notificationBuilder.setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
+        notificationBuilder.priority = NotificationCompat.PRIORITY_LOW
+        val notification = notificationBuilder.build()
+        ContextCompat.getSystemService(context, NotificationManager::class.java)
+            ?.notify(mediaSessionId, notification)
     }
 }
