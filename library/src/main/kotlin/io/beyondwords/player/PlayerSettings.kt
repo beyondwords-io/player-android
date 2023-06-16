fb496a714e85139bd2b8060458ec55515a2b1848
nklhtv <nklhtv@gmail.com>
2023-06-16 10:02:00 +0300

    add more props to PlayerSettings

--- library/src/main/kotlin/io/beyondwords/player/PlayerSettings.kt
+++ library/src/main/kotlin/io/beyondwords/player/PlayerSettings.kt
@@ -6,6 +6,8 @@ data class PlayerSettings(
     var contentId: String? = null,
     var playlistId: String? = null,
     var sourceId: String? = null,
+    var sourceUrl: String? = null,
+    var showUserInterface: Boolean? = null,
     var skipButtonStyle: String? = null,
     var playerStyle: String? = null
 )
