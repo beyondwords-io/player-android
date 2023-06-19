13c3c358ce5cc7085c103d3138023ead04cafcec
nklhtv <nklhtv@gmail.com>
2023-06-19 10:46:20 +0300

    add custom view to the example activity

--- example/app/src/main/kotlin/io/beyondwords/example/MainActivity.kt
+++ example/app/src/main/kotlin/io/beyondwords/example/MainActivity.kt
@@ -1,31 +1,32 @@
 package io.beyondwords.example
 
 import android.os.Bundle
-import android.util.Log
 import android.widget.AutoCompleteTextView
+import android.widget.FrameLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatButton
-import androidx.core.widget.addTextChangedListener
 import com.google.android.material.textfield.TextInputEditText
-import io.beyondwords.player.EventListener
-import io.beyondwords.player.PlayerEvent
 import io.beyondwords.player.PlayerSettings
 import io.beyondwords.player.PlayerView
 
 class MainActivity : AppCompatActivity() {
-    private var playerView: PlayerView? = null
+    private lateinit var playerContainerLayout: FrameLayout
+    private lateinit var playerUIInput: AutoCompleteTextView
     private lateinit var projectIdInput: TextInputEditText
     private lateinit var contentIdInput: TextInputEditText
     private lateinit var playlistIdInput: TextInputEditText
     private lateinit var skipButtonStyleInput: AutoCompleteTextView
     private lateinit var playerStyleInput: AutoCompleteTextView
     private lateinit var loadButton: AppCompatButton
+    private var defaultPlayerView: PlayerView? = null
+    private var customPlayerView: CustomPlayerView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        playerView = findViewById(R.id.player_view)
+        playerContainerLayout = findViewById(R.id.player_container_layout)
+        playerUIInput = findViewById(R.id.player_ui_input)
         projectIdInput = findViewById(R.id.project_id_input)
         contentIdInput = findViewById(R.id.content_id_input)
         playlistIdInput = findViewById(R.id.playlist_id_input)
@@ -33,36 +34,34 @@ class MainActivity : AppCompatActivity() {
         playerStyleInput = findViewById(R.id.player_style_input)
         loadButton = findViewById(R.id.load_button)
 
-        playerView?.addEventListener(object : EventListener {
-            override fun onPressedPlay(event: PlayerEvent) {
-                Log.d("PlayerView", "onPressedPlay($event)")
-            }
-
-            override fun onAny(event: PlayerEvent, settings: PlayerSettings) {
-                Log.d("PlayerView", "onAny($event, $settings)")
-            }
-        })
-
         loadButton.setOnClickListener { loadOnClick() }
-
-        skipButtonStyleInput.addTextChangedListener {
-            playerView?.setSkipButtonStyle(skipButtonStyleInput.text.toString())
-        }
-
-        playerStyleInput.addTextChangedListener {
-            playerView?.setPlayerStyle(playerStyleInput.text.toString())
-        }
     }
 
     private fun loadOnClick() {
-        playerView?.load(
-            PlayerSettings(
-                projectId = projectIdInput.text.toString(),
-                contentId = contentIdInput.text.toString(),
-                playlistId = playlistIdInput.text.toString(),
-                skipButtonStyle = skipButtonStyleInput.text.toString(),
-                playerStyle = playerStyleInput.text.toString()
-            )
+        playerContainerLayout.removeAllViews()
+        defaultPlayerView = null
+        customPlayerView = null
+        val settings = PlayerSettings(
+            projectId = projectIdInput.text.toString(),
+            contentId = contentIdInput.text.toString(),
+            playlistId = playlistIdInput.text.toString(),
+            skipButtonStyle = skipButtonStyleInput.text.toString(),
+            playerStyle = playerStyleInput.text.toString()
         )
+        when (playerUIInput.text.toString()) {
+            "default" -> {
+                defaultPlayerView = PlayerView(this).also {
+                    it.load(settings)
+                    playerContainerLayout.addView(it)
+                }
+            }
+
+            "custom" -> {
+                customPlayerView = CustomPlayerView(this).also {
+                    it.load(settings)
+                    playerContainerLayout.addView(it)
+                }
+            }
+        }
     }
 }
