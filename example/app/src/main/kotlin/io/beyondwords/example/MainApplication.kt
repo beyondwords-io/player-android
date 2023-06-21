224d4c2447499b9df843f162fc5eb7abf853b54f
nklhtv <nklhtv@gmail.com>
2023-06-21 14:10:21 +0300

    wrap the custom notification channel id with SDK check

--- example/app/src/main/kotlin/io/beyondwords/example/MainApplication.kt
+++ example/app/src/main/kotlin/io/beyondwords/example/MainApplication.kt
@@ -1,6 +1,7 @@
 package io.beyondwords.example
 
 import android.app.Application
+import android.os.Build
 import androidx.core.app.NotificationChannelCompat
 import androidx.core.app.NotificationManagerCompat
 import io.beyondwords.player.MediaSession
@@ -12,15 +13,17 @@ class MainApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        val channelBuilder = NotificationChannelCompat.Builder(
-            AUDIO_PLAYER_NOTIFICATION_CHANNEL_ID,
-            NotificationManagerCompat.IMPORTANCE_LOW
-        )
-        channelBuilder.setName("Audio Player")
-        channelBuilder.setDescription("Audio player notification channel description")
-        val channel = channelBuilder.build()
-        NotificationManagerCompat.from(this)
-            .createNotificationChannel(channel)
-        MediaSession.notificationChannelId = AUDIO_PLAYER_NOTIFICATION_CHANNEL_ID
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            val channelBuilder = NotificationChannelCompat.Builder(
+                AUDIO_PLAYER_NOTIFICATION_CHANNEL_ID,
+                NotificationManagerCompat.IMPORTANCE_LOW
+            )
+            channelBuilder.setName("Audio Player")
+            channelBuilder.setDescription("Audio player notification channel description")
+            val channel = channelBuilder.build()
+            NotificationManagerCompat.from(this)
+                .createNotificationChannel(channel)
+            MediaSession.notificationChannelId = AUDIO_PLAYER_NOTIFICATION_CHANNEL_ID
+        }
     }
 }
