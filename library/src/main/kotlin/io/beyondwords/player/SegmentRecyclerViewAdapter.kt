a08d8ae572652b2d4bc915d2c9a27365efa91078
Nikola Hristov <nklhtv@gmail.com>
2024-03-15 19:31:35 +0200

    added support for play from paragraph based on XPath and md5

--- library/src/main/kotlin/io/beyondwords/player/SegmentRecyclerViewAdapter.kt
+++ library/src/main/kotlin/io/beyondwords/player/SegmentRecyclerViewAdapter.kt
@@ -6,13 +6,14 @@ import androidx.annotation.RequiresApi
 import androidx.recyclerview.widget.RecyclerView
 
 @RequiresApi(Build.VERSION_CODES.N)
-abstract class SegmentRecyclerViewAdapter<T: SegmentRecyclerViewAdapter.SegmentViewHolder>(var playerView: PlayerView): RecyclerView.Adapter<T>() {
-    abstract class SegmentViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+abstract class SegmentRecyclerViewAdapter<T : SegmentRecyclerViewAdapter.SegmentViewHolder>(var playerView: PlayerView) :
+    RecyclerView.Adapter<T>() {
+    abstract class SegmentViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         var current = false
         var onSelect: (() -> Unit)? = null
     }
 
-    private val listener = object: EventListener {
+    private val listener = object : EventListener {
         override fun onEvent(event: PlayerEvent, settings: PlayerSettings) {
             if (event.type == "CurrentSegmentUpdated") {
                 notifyDataSetChanged()
@@ -26,13 +27,25 @@ abstract class SegmentRecyclerViewAdapter<T: SegmentRecyclerViewAdapter.SegmentV
 
     override fun onBindViewHolder(viewHolder: T, position: Int) {
         val segmentMarker = getSegmentMarker(position)
-        viewHolder.current = segmentMarker == playerView.currentSegment?.marker
+        val segmentXPath = getSegmentXPath(position)
+        val segmentMD5 = getSegmentMD5(position)
+        viewHolder.current = (segmentMarker != null && segmentMarker == playerView.currentSegment?.marker) ||
+                (segmentXPath != null && segmentXPath == playerView.currentSegment?.xpath) ||
+                (segmentMD5 != null && segmentMD5 == playerView.currentSegment?.md5)
         viewHolder.onSelect = {
-            playerView.setCurrentSegment(segmentMarker = segmentMarker)
+            playerView.setCurrentSegment(
+                segmentMarker = segmentMarker,
+                segmentMD5 = segmentMD5,
+                segmentXPath = segmentXPath
+            )
         }
     }
 
-    abstract fun getSegmentMarker(position: Int): String?
+    open fun getSegmentMarker(position: Int): String? = null
+
+    open fun getSegmentXPath(position: Int): String? = null
+
+    open fun getSegmentMD5(position: Int): String? = null
 
     fun release() {
         playerView.removeEventListener(listener)
